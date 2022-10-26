0df24e023d11b92c07b42ff1ddd851112e2216ab
bee-thedev <beenyshsaeed@gmail.com>
2022-10-26 22:28:28 +0500

    Update 7kyu.js

--- 7kyu/7kyu.js
+++ 7kyu/7kyu.js
@@ -9,7 +9,20 @@
 // solution('abc', 'd') // returns false
 
 function solution(str, ending){
-    
+    console.log(str.endsWith(ending))
   }
 
-  solution('abc', 'd')
+  solution('abcde', 'cde')
+
+  // ----------- Descending order 
+
+//   Your task is to make a function that can take any non-negative integer as an argument and ...
+//return it with its digits in descending order. Essentially, rearrange the digits to create the highest possible number.
+
+// Examples:
+// Input: 42145 Output: 54421
+
+// Input: 145263 Output: 654321
+
+// Input: 123456789 Output: 987654321
+
