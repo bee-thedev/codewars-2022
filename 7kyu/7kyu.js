3ff5e9e77b819fdf3c8c85ca212984cd98963176
bee-thedev <beenyshsaeed@gmail.com>
2022-11-05 23:27:31 +0500

    Update 7kyu.js

--- 7kyu/7kyu.js
+++ 7kyu/7kyu.js
@@ -84,3 +84,30 @@ function highAndLow(numbers){
 
 highAndLow("1 9 3 4 -5")
 
+//================================= Are they square? =================
+
+// DESCRIPTION:
+// Write a function that checks whether all elements in an array are square numbers. The function should be able to take any number of array elements.
+
+// Your function should return true if all elements in the array are square numbers and false if not.
+
+// An empty array should return undefined / None / nil /false (for C). You can assume that all array elements will be positive integers.
+
+// Examples:
+
+// isSquare([1, 4, 9, 16]) --> true
+
+// isSquare([3, 4, 7, 9]) --> false
+
+// isSquare([]) --> undefined
+// is_square([1, 4, 9, 16]) --> True
+
+// is_square([3, 4, 7, 9]) --> False
+
+// is_square([]) --> None
+
+var isSquare = function(arr){
+  //Your code here
+}
+
+isSquare([1, 4, 9, 16])
