e3e563d0eeab925158ec2cf1677191731dcfde7a
bee-thedev <beenyshsaeed@gmail.com>
2022-11-09 23:46:16 +0500

    Update 7kyu.js

--- 7kyu/7kyu.js
+++ 7kyu/7kyu.js
@@ -162,7 +162,15 @@ swap("Monday")
 // vowelOne( "aeiou, abc" ) // "1111100100"
 
 function vowelOne(s){
-  // ...
+    for(let i=0; i<s.length; i++){
+      if(s[i] === "a" || s[i] === "e" || s[i] === "i" || s[i] === "o" || s[i] === "u"){
+      s.replace(s[i], '1')
+      } else{
+  s.replace(s[i], '0')
+}
+return console.log(s)
+
+}
 }
 
-vowelOne()
+vowelOne("abceios")
