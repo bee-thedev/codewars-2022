1d94c0ce152591aba6b8486558d6fd38ca277b83
bee-thedev <beenyshsaeed@gmail.com>
2022-11-15 23:23:19 +0500

    7kyu-Testing123
    Unsolved yet

---
+++ 7kyu/7kyu-testing123.js
@@ -0,0 +1,34 @@
+// DESCRIPTION:
+// Your team is writing a fancy new text editor and you've been tasked with implementing the line numbering.
+
+// Write a function which takes a list of strings and returns each line prepended by the correct number.
+
+// The numbering starts at 1. The format is n: string. Notice the colon and space in between.
+
+// Examples: (Input --> Output)
+
+// [] --> []
+// ["a", "b", "c"] --> ["1: a", "2: b", "3: c"]
+
+var number=function(array){
+  
+
+}
+
+number(["a", "b", "c"])
+
+function solution(names) {
+  // javascript object
+  const values = {};
+
+  for(let i = 0; i < names.length; i++){
+      if(values[names[i]]){             
+         values[names[i]] = 1;
+      }else{
+         values[names[i]] += 1;
+      }
+  }
+  console.log(values);
+}
+
+solution(["a", "b", "c"])
