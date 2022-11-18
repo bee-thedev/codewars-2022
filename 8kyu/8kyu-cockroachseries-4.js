d5189321e73dcd447c6323bed82cb049bac0cd1d
bee-thedev <beenyshsaeed@gmail.com>
2022-11-18 20:43:48 +0500

    8kyu-Cockroach series added

---
+++ 8kyu/8kyu-cockroachseries-4.js
@@ -0,0 +1,18 @@
+// DESCRIPTION:
+// The cockroach is one of the fastest insects. 
+//Write a function which takes its speed in km per hour and returns it in cm per second, ..
+//rounded down to the integer (= floored).
+
+// For example:
+
+// 1.08 --> 30
+
+
+function cockroachSpeed(s) {
+    let conversion = (s*10**5)/3600
+    console.log(conversion)
+    let intoCmPerSecond = Math.floor(conversion)
+    console.log(intoCmPerSecond)
+  }
+
+  cockroachSpeed(1.08)
