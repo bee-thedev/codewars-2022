de956cdfc01a5dde0ac9871bf8cd29f18f6863be
bee-thedev <beenyshsaeed@gmail.com>
2022-11-22 22:23:36 +0500

    Update 7kyu-mathemagics.js

--- 7kyu/7kyu-mathemagics.js
+++ 7kyu/7kyu-mathemagics.js
@@ -19,16 +19,24 @@
 1 // Since 13 is at the 2nd column
 
 function guessTheCard(audience) {
-    /*
-    audience has the method getInput(), that receives an array of
-    3 arrays of 7 cards as parameter and returns the index of the
-    array where the selected card is present.
-    This method can be called up to 3 times,
-    after that it will throw an Error.
-    */
-
-
-        return 2;
+    let array = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21],
+        cards = [[], [], []],
+        count = 0
+    const deal = () =>
+        array.forEach((el, index) => cards[index % 3].push(el))    
+    const stack = () => {
+        let row = audience.getInput(cards)
+        row == 0 ? array = cards[1].concat(cards[0]).concat(cards[2])
+            : row == 1 ? array = cards[0].concat(cards[1]).concat(cards[2])
+                : array = cards[0].concat(cards[2]).concat(cards[1])
+                cards = [[],[],[]]
     }
+    while(count<3){
+        deal()
+        stack()
+        count++       
+    }
+    return array[10]
+}
 
     guessTheCard()
