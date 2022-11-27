7ad0bd3f576879c30dc7c1c8c10989a0a8adf72a
bee-thedev <beenyshsaeed@gmail.com>
2022-11-27 23:01:56 +0500

    Update 7kyu-next-pallindromic-number.js

--- 7kyu/7kyu-next-pallindromic-number.js
+++ 7kyu/7kyu-next-pallindromic-number.js
@@ -18,12 +18,17 @@
 
 // Enjoy it!!
 
-function nextPal(num) {
-        let reverse = num.toString().split("").reverse().join("")
-      console.log(reverse)
+function nextPal(val) {
+    //     let reverse = val.toString().split("").reverse().join("")
+    //   console.log(reverse)
+      function range(start, end) {
+        return Array(end - start + 1). fill(). map((_, idx) => start + idx)
+      }
+        var result = range(11, val); // [9, 10, 11, 12, 13, 14, 15, 16, 17, 18]
+        console. log(result);
 
-     
+        return console.log(result)
      }
    
 
-    nextPal(11)
+    nextPal(34)
