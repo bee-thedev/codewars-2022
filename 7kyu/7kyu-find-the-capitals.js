c5232784151a002991eeef01d484c0047dfb14cf
bee-thedev <beenyshsaeed@gmail.com>
2022-12-04 21:54:57 +0500

    7kyu - Find the Capital

---
+++ 7kyu/7kyu-find-the-capitals.js
@@ -0,0 +1,19 @@
+// Instructions
+// Write a function that takes a single string (word) as argument. The function must return an ordered list containing the indexes of all capital letters in the string.
+
+// Example
+// Test.assertSimilar( capitals('CodEWaRs'), [0,3,4,6] );
+
+
+var capitals = function (word) {
+     let split = word.split("")
+     console.log(split);
+
+     split.map(item=>{
+        return console.log(item.toUpperCase())
+     })
+
+     split.indexOf()
+}
+
+capitals('BeeNySh');
