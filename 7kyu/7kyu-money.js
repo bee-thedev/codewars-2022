a758d8b104d661ada39b0ba4ae62e30bc1ee5abe
Beenysh Sara <beenyshsaeed@gmail.com>
2023-01-01 23:27:39 +0500

    Create 7kyu-money.js

---
+++ 7kyu/7kyu-money.js
@@ -0,0 +1,10 @@
+Money, Money, Money
+Mr. Scrooge has a sum of money 'P' that he wants to invest. Before he does, he wants to know how many years 'Y' this sum 'P' has to be kept in the bank in order for it to amount to a desired sum of money 'D'.
+
+The sum is kept for 'Y' years in the bank where interest 'I' is paid yearly. After paying taxes 'T' for the year the new sum is re-invested.
+
+Note to Tax: not the invested principal is taxed, but only the year's accrued interest
+
+Example:
+
+  Let P be the Principal = 1000.00
